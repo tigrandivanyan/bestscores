159ca0a29384f134883af4c782265addd19d706a
Tigran Divanyan <tigrandivanyan2004@gmail.com>
2025-04-07 20:19:39 +0400

    updated eventName autoSaving

--- back/index.ts
+++ back/index.ts
@@ -6,7 +6,8 @@ import userRouter from "./routes/user";
 import http from "http";
 import { Server } from "socket.io";
 import User from "./models/User";
-import gameRouter from './routes/game'
+import gameRouter from "./routes/game";
+import Game from "./models/Game";
 
 configDotenv();
 
@@ -14,56 +15,56 @@ const app = express();
 const server = http.createServer(app);
 
 export const io = new Server(server, {
-	cors: {
-		origin: "*", // Change this to the actual origin for production
-		methods: ["GET", "POST"],
-		credentials: true
-	}
+    cors: {
+        origin: "*", // Change this to the actual origin for production
+        methods: ["GET", "POST"],
+        credentials: true,
+    },
 });
 
 interface IConnectedUser {
-	token: string;
-	socketId: string;
+    token: string;
+    socketId: string;
 }
 
 let connectedUsers: Array<IConnectedUser> = [];
 
 io.use((socket, next) => {
-	const token = socket.handshake.query.token;
+    const token = socket.handshake.query.token;
 
-	if (!token) {
-		return next(new Error("Authentication error"));
-	}
+    if (!token) {
+        return next(new Error("Authentication error"));
+    }
 
-	(async () => {
-		const user = await User.findOne({ token: token });
+    (async () => {
+        const user = await User.findOne({ token: token });
 
-		//@ts-ignore
-		socket.user = user;
+        //@ts-ignore
+        socket.user = user;
 
-		if (user?.permissions.sender || !user?.permissions.sender) {
-			next();
+        if (user?.permissions.sender || !user?.permissions.sender) {
+            next();
         }
         if (!user?.permissions.all && !user?.permissions.sender) {
-            return next(new Error("dont have permission"))
+            return next(new Error("dont have permission"));
         }
-	})();
+    })();
 });
 
 io.on("connection", (socket) => {
     console.log("A user connected: ", socket.id);
-    
+
     //@ts-ignore
-    const existedUser = connectedUsers.find((user: IConnectedUser) => user.token === socket.user.token)
+    const existedUser = connectedUsers.find((user: IConnectedUser) => user.token === socket.user.token);
     if (existedUser) {
-        const oldSocket = io.sockets.sockets.get(existedUser.socketId)
+        const oldSocket = io.sockets.sockets.get(existedUser.socketId);
         if (oldSocket) {
-            console.log("User disconnected from old socket")
-            oldSocket.disconnect(true)
+            console.log("User disconnected from old socket");
+            oldSocket.disconnect(true);
         }
 
         //@ts-ignore
-        connectedUsers = connectedUsers.filter(user => user.token !== socket.user.token);
+        connectedUsers = connectedUsers.filter((user) => user.token !== socket.user.token);
     }
 
     socket.join("room1");
@@ -71,22 +72,33 @@ io.on("connection", (socket) => {
     connectedUsers.push({
         //@ts-ignore
         token: socket.user.token,
-        socketId: socket.id
-    })
+        socketId: socket.id,
+    });
 
     socket.on("message", (message) => {
         //@ts-ignore
         if (socket.user.permissions.sender) {
-			io.to("room1").emit("message", message);
-		} else {
-			//@ts-ignore
-			console.log(`User ${socket.user.id} tried to send a message without permission`);
-		}
-	});
+            io.to("room1").emit("message", message);
+            let eventName;
+            try {
+                eventName = JSON.parse(message).eventName;
+            } catch {
+                eventName = message.eventName;
+            }
+            Game.findOne({ eventName }).then((game) => {
+                if (!game) {
+                    new Game({ eventName, active: true }).save().then(() => console.log("saved new game " + eventName));
+                }
+            });
+        } else {
+            //@ts-ignore
+            console.log(`User ${socket.user.id} tried to send a message without permission`);
+        }
+    });
 
-	socket.on("disconnect", () => {
-		console.log("User disconnected: ", socket.id);
-	});
+    socket.on("disconnect", () => {
+        console.log("User disconnected: ", socket.id);
+    });
 });
 
 const PORT = process.env.PORT || 8000;
@@ -94,28 +106,28 @@ const PORT = process.env.PORT || 8000;
 const MONGO_DB = process.env.MONGO_DB;
 
 if (!MONGO_DB) {
-	throw new Error("No MONGO_DB in env");
+    throw new Error("No MONGO_DB in env");
 }
 
 // Connect to MongoDB
 mongoose
-	.connect(MONGO_DB)
-	.then(() => console.log("Connected to MongoDB"))
-	.catch((error) => console.error("MongoDB connection error:", error));
+    .connect(MONGO_DB)
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((error) => console.error("MongoDB connection error:", error));
 
 // Middleware
 app.use(express.json());
 app.use(
-	cors({
-		origin: "*",
-	})
+    cors({
+        origin: "*",
+    })
 );
 
 // Routes
 app.use("/api/user", userRouter);
-app.use("/api/game", gameRouter)
+app.use("/api/game", gameRouter);
 
 // Start the server
 server.listen(PORT, () => {
-	console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
